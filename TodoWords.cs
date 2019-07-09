c6c01cb0b872807e297f7118cba39b656cd7ae9f
SogoProds <sogoprods@gmail.com>
2019-07-09 16:29:39 +0300

    Created a Models folder and TodoWords.cs to it.

---
+++ TodoWords.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TodoAPI.Models
+{
+    public class TodoWords
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Difficulty { get; set; }
+    }
+}

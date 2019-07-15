dec965b56ac0055dcd45100140ea54a32df2735a
SogoProds <sogoprods@gmail.com>
2019-07-15 21:05:44 +0300

    Merging everything into one. Easier for me.

---
+++ WebHang.Data/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebHang.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Word> Words { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Highscore>
+    }
+}
--- WebHang.Models/Highscore.cs
+++ WebHang.Models/Highscore.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebHang.Models
 {
-    class Highscores
+    class Highscore
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
